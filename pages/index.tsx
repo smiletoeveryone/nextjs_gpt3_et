743ea61b66734408134d2a3ad72e5bac6dfb32a2
fiftycentsjj <fiftycentsjj@gmail.com>
2023-03-08 15:08:26 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,77 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import mainImage from '@/assets/images/lionfb_01.jpg'
+import { Form, Button, Spinner } from 'react-bootstrap'
+import { FormEvent, useState } from 'react'
+
+export default function Home() {
+
+  const [quote, setQuote] = useState("");
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const prompt = formData.get("prompt")?.toString().trim();
+
+    if (prompt) {
+      try {
+        setQuote("");
+        setQuoteLoadingError(false);
+        setQuoteLoading(true);
+
+        const response = await fetch("/api/cringe?prompt=" + encodeURIComponent(prompt));
+        const body = await response.json();
+        setQuote(body.quote);
+      } catch (error) {
+        console.error(error);
+        setQuoteLoadingError(true);
+      } finally {
+        setQuoteLoading(false);
+      }
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>ChatGPT_AI_開發專案</title>
+        <meta name="description" content="by Coding in Flow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="" />
+      </Head>
+      <main className={styles.main}>
+        <h1>ChatGPT_AI_開發專案🪅️</h1>
+        <h2>Create by Scott</h2>
+        <div>AI 對話、文本產生平臺</div>
+        <div className={styles.mainImageContainer}>
+          <Image
+            src={mainImage}
+            fill
+            alt='ET'
+            priority
+            className={styles.mainImage}
+          />
+        </div>
+        <Form onSubmit={handleSubmit} className={styles.inputForm}>
+          <Form.Group className='mb-3' controlId='prompt-input'>
+            <Form.Label>Hi！您好：我是AI助理，歡迎詢問任何問題❗️😋️</Form.Label>
+            <Form.Control
+              name='prompt'
+              placeholder='請在此輸入您的問題...'
+              maxLength={100}
+            />
+          </Form.Group>
+          <Button type='submit' className='mb-3' disabled={quoteLoading}>
+            Say
+          </Button>
+        </Form>
+        {quoteLoading && <Spinner animation='border' />}
+        {quoteLoadingError && "Something went wrong. Please try again."}
+        {quote && <h5>{quote}</h5>}
+      </main>
+    </>
+  )
+}
